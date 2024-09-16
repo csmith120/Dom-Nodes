d7e2c41cc6bd01621f505059b9373e3bb23c52d7
csmith120 <taskmaster3654@gmail.com>
2024-09-16 13:36:43 -0500

    fixed exercise 1

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@ document.getElementById("myDiv").style.backgroundColor = "lightblue";
 const paragraphs = document.querySelectorAll("p");
 
 paragraphs.forEach(paragraph => {
-    console.log(paragraphs.innerText)
+    paragraph
+    console.log(paragraph.innerText)
 });
